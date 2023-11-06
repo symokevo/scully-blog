1f84f1b94cb8a05336487dd24b6da0919ab296f9
Simon Rabuogi <symokevo@gmail.com>
2023-11-06 06:53:50 +0300

    Configure Contact Page Routing
    Note: When you design your website and need to decide upon the Angular modules that
    you will use, check out the main menu of the website. Each link of the menu should
    be a different feature and, thus, a different Angular module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/header/header.component'
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,10 @@ import { HeaderComponent } from './core/header/header.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
