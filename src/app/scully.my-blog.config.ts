8b8cf5f14757d1782cd7ff10778bd1aea2a235eb
Simon Rabuogi <symokevo@gmail.com>
2023-11-07 04:21:46 +0300

    Install the Scully Library
    - use 'ng add @scullyio/init'
    - Import the library in your main modules file
    - Create a configuration file 'scully.projectName.config.ts' and update it accordingly.
    NOTE: The Scully output path must be different from the path that the Angular CLI outputs for the bundle of your Angular Application
        The latter can be configured in the angular.json file.

---
+++ src/app/scully.my-blog.config.ts
@@ -0,0 +1,11 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'scully-blog',
+  // distFolder: './dist/[PROJECT_NAME]', 
+  // Up here is the output directory of your Angular build artifacts
+  outDir: './dist/static', // directory for scully build artifacts
+  // defaultPostRenderers: [],
+  routes: {},
+};
