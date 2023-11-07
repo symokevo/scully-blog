352754142fa9319086f59d392b888f2b76f30079
Simon Rabuogi <symokevo@gmail.com>
2023-11-07 06:07:27 +0300

    Add scully configuration for displaying posts in the main page.

--- scully.scully-blog.config.ts
+++ scully.scully-blog.config.ts
@@ -7,5 +7,11 @@ export const config: ScullyConfig = {
   // Up here is the output directory of your Angular build artifacts
   outDir: './dist/static', // directory for scully build artifacts
   // defaultPostRenderers: [],
-  routes: {},
+  routes: {
+    '/posts/:id': {
+      type: 'contentFolder',
+      id: {
+        folder: "./mdfiles"
+      }
+    },},
 };
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent {
-
+export class ArticlesComponent implements OnInit {
+  constructor(private scullyService: ScullyRoutesService) {}
+  posts$: Observable<ScullyRoute[]> | undefined;
+  ngOnInit(): void {
+    this.posts$ = this.scullyService.available$.pipe(
+      map(posts => posts.filter(post => post.title))
+    );
+  }
 }
